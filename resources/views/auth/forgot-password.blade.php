48b5e62b250a110995ac892f9a540466eab0885f
laraveller <laraveller2021@gmail.com>
2021-04-04 13:30:28 +0200

    first commit

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,36 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+               <img src="{{asset('img/logo.png')}}" class="w-14 h-14 rounded">
+            </a>
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-300">
+            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        </div>
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
