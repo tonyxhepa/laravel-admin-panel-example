936cfcd9851e38befd8fbb7c8dd82349e6d25313
laraveller <laraveller2021@gmail.com>
2021-04-07 19:22:06 +0200

    create posts

--- public/js/app.js
+++ public/js/app.js
@@ -3802,6 +3802,66 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 __webpack_require__(/*! alpinejs */ "./node_modules/alpinejs/dist/alpine.js");
 
+function data() {
+  function getThemeFromLocalStorage() {
+    // if user already changed the theme, use it
+    if (window.localStorage.getItem('dark')) {
+      return JSON.parse(window.localStorage.getItem('dark'));
+    } // else return their preferences
+
+
+    return !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  function setThemeToLocalStorage(value) {
+    window.localStorage.setItem('dark', value);
+  }
+
+  return {
+    dark: getThemeFromLocalStorage(),
+    toggleTheme: function toggleTheme() {
+      this.dark = !this.dark;
+      setThemeToLocalStorage(this.dark);
+    },
+    isSideMenuOpen: false,
+    toggleSideMenu: function toggleSideMenu() {
+      this.isSideMenuOpen = !this.isSideMenuOpen;
+    },
+    closeSideMenu: function closeSideMenu() {
+      this.isSideMenuOpen = false;
+    },
+    isNotificationsMenuOpen: false,
+    toggleNotificationsMenu: function toggleNotificationsMenu() {
+      this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen;
+    },
+    closeNotificationsMenu: function closeNotificationsMenu() {
+      this.isNotificationsMenuOpen = false;
+    },
+    isProfileMenuOpen: false,
+    toggleProfileMenu: function toggleProfileMenu() {
+      this.isProfileMenuOpen = !this.isProfileMenuOpen;
+    },
+    closeProfileMenu: function closeProfileMenu() {
+      this.isProfileMenuOpen = false;
+    },
+    isPagesMenuOpen: false,
+    togglePagesMenu: function togglePagesMenu() {
+      this.isPagesMenuOpen = !this.isPagesMenuOpen;
+    },
+    // Modal
+    isModalOpen: false,
+    trapCleanup: null,
+    openModal: function openModal() {
+      this.isModalOpen = true;
+      this.trapCleanup = focusTrap(document.querySelector('#modal'));
+    },
+    closeModal: function closeModal() {
+      this.isModalOpen = false;
+      this.trapCleanup();
+    }
+  };
+}
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,45 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+    <div class="py- m-6">
+         <div class="w-full m-2">
+                <a href="#" class="m-2 p-2 bg-green-400 rounded-lg">
+                    Create Post
+                </a>
+            </div>
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+           
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Id</th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Title</th>
+                    <th scope="col" class="relative px-6 py-3">Edit</th>
+                </tr>
+                </thead>
+                <tbody class="bg-white dark:bg-gray-700 divide-y divide-gray-200">
+                <tr></tr>
+                @foreach (App\Models\Post::all() as $post)
+                      <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $post->id }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $post->title}}</td>
+                    <td class="px-6 py-4 text-right text-sm">Edit Delete</td>
+                </tr> 
+                @endforeach
+             
+                <!-- More items... -->
+                </tbody>
+            </table>
+            <div class="m-2 p-2">Pagination</div>
             </div>
         </div>
+        </div>
+
     </div>
 </x-app-layout>
